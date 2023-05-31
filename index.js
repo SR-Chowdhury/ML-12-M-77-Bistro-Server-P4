0740fa1d10400e08ac6ea1b1de34ea8943a0a624
Shihan Chowdhury <hackedgdown@gmail.com>
2023-05-31 19:16:42 +0100

    77-8 Display cart items to review before order done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -67,6 +67,13 @@ async function run() {
             const item = req.body;
             // console.log(item);
             const result = await cartCollection.insertOne(item);
+            res.send(result);
+        });
+
+        app.delete('/carts:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         });
 
